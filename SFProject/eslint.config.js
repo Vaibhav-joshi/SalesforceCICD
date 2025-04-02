df8824b73b57afe951772803c776b605c8fe20d5
Vaibhav Joshi <vaibhav13590@gmail.com>
2025-04-02 17:30:20 +1100

    update

--- SFProject/eslint.config.js
+++ SFProject/eslint.config.js
@@ -1,21 +1,20 @@
-import { Linter } from '@eslint/eslintrc';
-
-export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
+module.exports = [
+    {
+      files: ["**/*.js"],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+      plugins: {
+        import: require("eslint-plugin-import"),
+        jest: require("eslint-plugin-jest"),
+        lwc: require("@lwc/eslint-plugin-lwc"),
+      },
+      rules: {
+        "import/no-unresolved": "error",
+        "jest/no-disabled-tests": "warn",
+        "semi": ["error", "always"],
+      },
     },
-    plugins: {
-      import: require("eslint-plugin-import"),
-      jest: require("eslint-plugin-jest"),
-      lwc: require("@lwc/eslint-plugin-lwc"),
-    },
-    rules: {
-      "import/no-unresolved": "error",
-      "jest/no-disabled-tests": "warn",
-      "semi": ["error", "always"],
-    },
-  },
-];
+  ];
+  
