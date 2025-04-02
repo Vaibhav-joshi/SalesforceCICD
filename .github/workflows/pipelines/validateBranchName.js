9baf85aeb97177efac0e4c967ed4ab77d7123455
Vaibhav Joshi <vaibhav13590@gmail.com>
2025-04-02 13:22:32 +1100

    validate

--- .github/workflows/pipelines/validateBranchName.js
+++ .github/workflows/pipelines/validateBranchName.js
@@ -1,30 +1,16 @@
-// Get the branch name from environment variables
 const branchName = process.env.BRANCH_NAME;
+const username = branchName.split("/")[1];
 
-// Define the required regex pattern
+const usernamePattern = /^[a-zA-Z]+\.[a-zA-Z]+$/;
 const branchPattern = /^feature\/[a-zA-Z]+\.[a-zA-Z]+\/US[0-9]+$/;
 
 // Validate the branch name format
-if (branchPattern.test(branchName)) {
+if (branchPattern.test(branchName) && usernamePattern.test(username)) {
   console.log(`Branch name is valid: ${branchName}`);
 } else {
   console.error(
     `Invalid branch name: ${branchName}\n` +
       `Expected format: feature/firstname.lastname/US{number}`
-  );
-  process.exit(1); // Exit with failure
-}
-
-// Check for the presence of a username
-const usernamePattern = /^[a-zA-Z]+\.[a-zA-Z]+$/; // Pattern for firstname.lastname
-const username = branchName.split("/")[1]; // Extract 'firstname.lastname'
-console.log(`Username is valid: ${username}`);
-
-if (usernamePattern.test(username)) {
-  console.log(`Username is valid: ${username}`);
-} else {
-  console.error(
-    `Invalid username: ${username}\nExpected format: firstname.lastname`
   );
   process.exit(1); // Exit with failure
 }
