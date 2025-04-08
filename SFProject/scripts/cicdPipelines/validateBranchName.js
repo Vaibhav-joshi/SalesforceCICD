7c02ce39883d8432468a1874e137c80da9a2ddb8
Vaibhav Joshi <vaibhav13590@gmail.com>
2025-04-08 14:07:24 +1000

    setup

---
+++ SFProject/scripts/cicdPipelines/validateBranchName.js
@@ -0,0 +1,16 @@
+const branchName = process.env.BRANCH_NAME;
+const username = branchName.split("/")[1];
+
+const usernamePattern = /^[a-zA-Z]+\.[a-zA-Z]+$/;
+const branchPattern = /^feature\/[a-zA-Z]+\.[a-zA-Z]+\/US[0-9]+$/;
+
+// Validate the branch name format
+if (branchPattern.test(branchName) && usernamePattern.test(username)) {
+  console.log(`Branch name is valid: ${branchName}`);
+} else {
+  console.error(
+    `Invalid branch name: ${branchName}\n` +
+      `Expected format: feature/firstname.lastname/US{number}`
+  );
+  process.exit(1); // Exit with failure
+}
